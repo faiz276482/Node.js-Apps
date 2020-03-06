83e944b4f9a81fda35667f9d3a2f189ad4767747
Faiz Alam <faiz276482@gmail.com>
2020-03-06 18:11:54 +0530

    Added yargs module to parse user input through CLI and Customized the version Defined custom commands

--- Notes-App/app.js
+++ Notes-App/app.js
@@ -1,16 +1,64 @@
 // const validator=require('validator');
 const chalk=require("chalk");
+const yargs=require('yargs');
 const getNotes=require("./notes.js");
 
-console.log(getNotes());
+//Customize yargs version
+yargs.version("1.1.0");
 
-console.log(chalk.red.bgGreen.bold('Failure!'));
+//Create add command
+
+yargs.command({
+    command:"add",
+    describe:"Add new note",
+    handler: function(){
+        console.log("Adding a new note!");
+    }
+})
+
+//Create remove command
+
+yargs.command({
+    command:"remove",
+    describe:"Removing a note",
+    handler: function(){
+        console.log("Removing a note!");
+    }
+})
+
+//Create List command
+
+yargs.command({
+    command:"list",
+    describe:"Listing all notes",
+    handler: function(){
+        console.log("Listing all notes!");
+    }
+})
+
+//Create read command
+
+yargs.command({
+    command:"read",
+    describe:"reading a note",
+    handler: function(){
+        console.log("Reading a note!");
+    }
+})
+
+// add , remove , list , read
 
 
-// console.log("IS Email?"+validator.isEmail("faiz276482@gmail.com"));
-// console.log("is URL?:"+validator.isURL("https://nerdytech.in"));
+console.log(yargs.argv);
 
 
 
-// const add=require("./utils.js");
-// console.log(add(5,5));
+// if(command==="add"){
+//     console.log("Adding Note");
+// }
+// else if(command==="remove"){
+//     console.log("Removing note");
+// }
+// else{
+//     console.log("Command not recognized");
+// }
