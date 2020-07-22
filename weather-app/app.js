d58ab72a006587bcfa707c1d329bff9d1a473e16
Faiz Alam <faiz276482@gmail.com>
2020-07-22 23:57:50 +0530

    Played with HTTP request to fetch weather data of a location

--- weather-app/app.js
+++ weather-app/app.js
@@ -1,11 +1,25 @@
-console.log("Starting");
+//video 1 and 2
+// console.log("Starting");
 
-setTimeout(()=>{
-    console.log("2 seconds timer");
-},2000);
+// setTimeout(()=>{
+//     console.log("2 seconds timer");
+// },2000);
 
-setTimeout(()=>{
-    console.log("0 seconds timer");
-},0);
+// setTimeout(()=>{
+//     console.log("0 seconds timer");
+// },0);
 
-console.log("Ending");
+// console.log("Ending");
+
+//=================================================================================//
+
+
+//video 3
+const request= require('request')
+
+const url='https://api.darksky.net/forecast/b5585121dd41caa42a06d9b7576e96c8/37.8267,-122.423'
+
+request({ url: url},(error,response) =>{
+    const data=JSON.parse(response.body)
+    console.log(data.currently);
+});
